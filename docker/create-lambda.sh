8af135be2e55c96492a58e90370e6dd3d863a99a
Shinji Yamada <dotneet@gmail.com>
2023-05-21 04:56:41 +0900

    fix

--- docker/create-lambda.sh
+++ docker/create-lambda.sh
@@ -4,6 +4,7 @@ claudia create \
         --region ${AWS_REGION} \
         --name ${LAMBDA_NAME} \
         --handler index.handler \
+        --arch arm64 \
         --runtime "nodejs16.x" \
         --role ${LAMBDA_ROLE} \
         --memory ${LAMBDA_MEMORY} \
